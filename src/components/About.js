c885992a6024fe610dc286a3dc0f2e5070d94aaf
divyanshukr96 <divyanshu461@gmail.com>
2019-05-01 03:56:21 +0530

    about section updated

--- src/components/About.js
+++ src/components/About.js
@@ -9,107 +9,81 @@ const About = () => {
     return (
         <div>
             <Navbar/>
-            <br/><br/>  <br/><br/>   <br/><br/>   <br/><br/>
+            <div className="afterNav"/>
             <MDBContainer fluid>
                 <MDBRow>
                     <MDBCol md="6" lg="6">
                         <MDBAnimation type="fadeInUp" duration="2s">
-                            <MDBView hover zoom className="">
+                            <MDBView hover zoom >
                                 <img
                                     src={Harinder}
-                                    alt=""
-                                    width={650}
-                                    height={450}
-                                    style={{padding:80,marginLeft:20}}
+                                    alt={"harinder"}
+                                    width={450}
+                                    style={{padding: 5, maxWidth: '100%'}}
                                 />
                             </MDBView>
                         </MDBAnimation>
-
-
                     </MDBCol>
-
                     <MDBCol lg="6" md="6" className="mb-4">
-
                         <MDBAnimation type="fadeInRight" duration="2s">
-                            <br/>
-                            <br/>
-                            <br/>
                             <blockquote className="blockquote bq-warning">
                                 <p className="bq-title">A little about me</p>
-                                <p style={{textAlign:"justify"}}>In 2017, I quit my day job to pursue my passion as a full time photographer and I am getting inspired every day for a new picture.
-
-                                    I am tempted towards nature and love to travel, my camera comes with me too! I try to capture as many beautiful moments as possible and create new challenges for myself.
-
-                                    Another thing that simulates me is emotions, I love to capture moments of joy, deep love and an honest smile. Wedding photography helps me flow these emotions and portray them on the best canvas.
-
+                                <p style={{textAlign: "justify"}}>
+                                    In 2017, I quit my day job to pursue my passion as a full time photographer and I am
+                                    getting inspired every day for a new picture. I am tempted towards nature and love
+                                    to travel, my camera comes with me too! I try to capture as many beautiful moments
+                                    as possible and create new challenges for myself. Another thing that simulates me is
+                                    emotions, I love to capture moments of joy, deep love and an honest smile. Wedding
+                                    photography helps me flow these emotions and portray them on the best canvas.
                                 </p>
                             </blockquote>
                         </MDBAnimation>
-
                     </MDBCol>
                 </MDBRow>
 
-                <br/>
-                <br/>
-
                 <MDBRow>
-                    <MDBCol md="6" lg="6">
-
-                        <MDBRow>
-                            <MDBAnimation reveal type="fadeInUp" duration="2s">
-                            <br/>
-                            <br/>
-                            <br/>
+                    <MDBCol md="6">
+                        <MDBAnimation type="fadeInLeft" duration="2s">
                             <blockquote className="blockquote bq-warning">
                                 <p className="bq-title">My vision</p>
-                                <p style={{textAlign:"justify"}}>
-                                    Love, passion and care that a couple exhibits, the best way to show them in prewedding photography in which I take pride.
-                                    I’ve always loved taking pictures as far as I can remember. It’s one of the most simple ways of capturing emotion onto a frame.
-                                    I love helping people create new memories or restore old ones. Providing beautiful images along with exceptional service to my
-                                    clients is what drives me! I love the outdoors and photography makes you want to get out and see the world at different angles.
-
+                                <p style={{textAlign: "justify"}}>
+                                    Love, passion and care that a couple exhibits, the best way to show them in
+                                    prewedding photography in which I take pride. I’ve always loved taking pictures
+                                    as far as I can remember. It’s one of the most simple ways of capturing emotion
+                                    onto a frame. I love helping people create new memories or restore old ones.
+                                    Providing beautiful images along with exceptional service to my clients is what
+                                    drives me! I love the outdoors and photography makes you want to get out and see
+                                    the world at different angles.
+                                </p>
+                            </blockquote>
+                        </MDBAnimation>
+                        <MDBAnimation type="fadeInUp" duration="2s">
+                            <blockquote className="blockquote">
+                                <p style={{textAlign: "justify"}} className="mb-0">
+                                    I live in Montreal, in the heart of downtown but my roots belong to India. I
+                                    graduated from Concordia university with Industrial engineering degree however I
+                                    always had a pull towards photography. During undergrad, I have done professional
+                                    pre-wedding photoshoots and landscapes. In 2017, I decided to pursue a full-time
+                                    career in photography. I got married in 2018 to my better half Raman Nanrhay, who
+                                    challenges me to be a better photographer every day.
                                 </p>
                             </blockquote>
                         </MDBAnimation>
 
-                        </MDBRow>
-
-                        <MDBRow>
-
-                            <MDBAnimation reveal type="fadeInUp" duration="2s">
-
+                        <MDBAnimation type="fadeInLeft" duration="2s">
                             <blockquote className="blockquote">
-                                <p style={{textAlign:"justify"}} className="mb-0">
-                                    I live in Montreal, in the heart of downtown but my roots belong to India.
-                                    I graduated from Concordia university with Industrial engineering degree however I always had a pull towards photography.
-                                    During undergrad, I have done professional pre-wedding photoshoots and landscapes.
-                                    In 2017, I decided to pursue a full-time career in photography.
-                                    I got married in 2018  to my better half Raman Nanrhay, who challenges me to be a better photographer every day.</p>
-                            </blockquote>
-                            </MDBAnimation>
-
-                        </MDBRow>
-
-                        <MDBRow>
-
-                            <MDBAnimation reveal type="fadeInLeft" duration="2s">
-                            <blockquote className="blockquote">
-                                <p  className="mb-0 abouttext">
-                                    There is always something to learn in photography. I keep myself updated with new technology, technique or new hacks to try.
+                                <p className="mb-0 abouttext">
+                                    There is always something to learn in photography. I keep myself updated with
+                                    new technology, technique or new hacks to try.
                                     Crafting a story for my clients is what I was born to do!
                                     I am excited about being part of your life!</p>
-                                <footer className="blockquote-footer mb-3">Harinder Singh <cite title="Source Title"></cite></footer>
+                                <footer className="blockquote-footer mb-3">Harinder Singh <cite title="Source Title"/>
+                                </footer>
                             </blockquote>
-                            </MDBAnimation>
-
-                        </MDBRow>
-
-
+                        </MDBAnimation>
                     </MDBCol>
-
                     <MDBCol lg="6" md="6" className="mb-4">
-
-                        <MDBAnimation reveal type="fadeInRight" duration="2s">
+                        <MDBAnimation type="fadeInUp" duration="2s">
                             <MDBView hover zoom className="">
                                 <img
                                     src={HarinderWife}
@@ -118,20 +92,13 @@ const About = () => {
                                     width={600}
                                     height={300}
                                 />
-                                <MDBMask className="flex-center">
-                                    <p className="white-text"></p>
-                                </MDBMask>
+                                <MDBMask className="flex-center"><p className="white-text"/></MDBMask>
                             </MDBView>
                         </MDBAnimation>
-
-
-
                     </MDBCol>
                 </MDBRow>
-
-
             </MDBContainer>
-        <Footer reveal={false}/>
+            <Footer/>
         </div>
     );
 };
