f1c47b9ffbe43e005643cda86c809740967247d8
divyanshukr96 <divyanshu461@gmail.com>
2019-05-01 03:23:18 +0530

    Navbar responsive done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import Logo from './../images/hsnlogo1.png';
+import '../menu.css'
 
-export default () => (
+class NavBar extends Component {
+    menuChange = () => {
+        let menuIcon = document.getElementById('menuIcon');
+        let menu = document.getElementById('menu');
+        menuIcon.classList.toggle("change");
+        menu.classList.toggle("mobile");
+    };
 
-       <div className="bar" style={{marginTop:20}}>
+    render() {
+        return (
+            <div className="barOuter">
+                <div className="logo"><img src={Logo} alt="logo" style={{width: '100%'}}/></div>
+                <div className="menuContainer" id="menuIcon" onClick={this.menuChange}>
+                    <div className="bar1"/>
+                    <div className="bar2"/>
+                    <div className="bar3"/>
+                </div>
+                <div className="bar mobile" id="menu">
+                    <Link to={'/'}><h1 className="cursive">Home</h1></Link>
+                    <Link to={'/gallery'}><h1 className="cursive">Gallery</h1></Link>
+                    <Link to={'/'}><h1 className="cursive" style={{display: 'none'}}>nul</h1></Link>
+                    <Link to={'/about'}><h1 className="cursive">About</h1></Link>
+                    <Link to={'/contact'}><h1 className="cursive">Contact</h1></Link>
+                </div>
+            </div>
+        );
+    }
+}
 
-           <Link to={'/'}> <h1 className="cursive">Home</h1> </Link>
-           <Link to={'/gallery'}> <h1 className="cursive">Gallery</h1> </Link>
-
-           <img
-               src={Logo}
-               alt=""
-               width={300}
-               height={200}
-           />
-
-           <Link to={'/about'}> <h1 className="cursive">About</h1> </Link>
-           <Link to={'/contact'}> <h1 className="cursive">Contact</h1> </Link>
-       </div>
-
-);
+export default NavBar;
